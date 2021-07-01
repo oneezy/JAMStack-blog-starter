5ff8a42e44cce3d5a5fa5dda34d91f386707a548
Justin O'Neill <justin@oneezy.com>
2021-06-30 21:21:25 -0500

    fixed typo

--- .eleventy.js
+++ .eleventy.js
@@ -8,12 +8,12 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addFilter('postDate', (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
-  })
+  });
 
   return {
     dir: {
       input: "src",
       output: "public"
     }
-  }
+  };
 }
